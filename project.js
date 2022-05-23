911c8b33133e981b940aafb6ce98959a7e4a7731
Alvin Nguyen <alvinonguyen00@gmail.com>
2022-05-23 11:06:59 -0700

    Rubik's Cube class first draft

--- project.js
+++ project.js
@@ -1,10 +1,13 @@
 import {defs, tiny} from './examples/common.js';
+import {rubiks} from './rubiks.js';
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
 } = tiny;
 
-const {Cube, Axis_Arrows, Textured_Phong} = defs
+const {Cube, Axis_Arrows, Textured_Phong} = defs;
+
+const {Rubiks} = rubiks;
 
 export class Project extends Scene {
     
